eabe81faff0f0491f44e819c7fdd85047d3498f0
Sandeep Reddy <mandasandeepreddy18@gmail.com>
2025-10-04 19:07:34 +0530

    test: Change message to verify deployment

--- app.py
+++ app.py
@@ -6,7 +6,8 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    return f"<h1>Hello from GitHub Actions!</h1><p>This page was updated at: {now}</p>"
+    # return f"<h1>Hello from GitHub Actions!</h1><p>This page was updated at: {now}</p>"
+    return f"<h1>It works! Auto-deployed from GitHub at: {now}</h1>"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5003)
