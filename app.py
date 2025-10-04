4257d64dc0fa175c96c3eb0bc821eb353df9dd57
Sandeep Reddy <mandasandeepreddy18@gmail.com>
2025-10-04 19:58:24 +0530

    appy

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 def hello():
     now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     # Change the message one more time
-    return f"<h1>SUCCESS! Fully automated deployment works! Timestamp: {now}</h1>"
+    return f"<h1>Finally SUCCESS! Fully automated deployment works! Timestamp: {now}</h1>"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5003)
